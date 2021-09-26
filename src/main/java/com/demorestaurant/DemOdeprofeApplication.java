54246841b6e9f20306108f064d8988aff80d80d1
Renzo <rsancheze1@upao.edu.pe>
2021-09-25 20:02:44 -0500

    Inicio del proyecto

---
+++ src/main/java/com/demorestaurant/DemOdeprofeApplication.java
@@ -0,0 +1,13 @@
+package com.demorestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemOdeprofeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemOdeprofeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demorestaurant/DemOdeprofeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demorestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemOdeprofeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
